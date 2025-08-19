01c7eb8b95d9a1362658898c627550a5c38f0cd7
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2025-08-19 16:17:28 +0600

    Update InstallOrganogram.php file to resolved conflict

--- src/Console/Commands/InstallOrganogram.php
+++ src/Console/Commands/InstallOrganogram.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
-
->>>>>>> d0767ef1debaab9034c94637e64674374542e4fb
 declare(strict_types=1);
 
 namespace Sanwarul\Organogram\Console\Commands;
@@ -18,22 +14,11 @@ class InstallOrganogram extends Command
     public function handle()
     {
         $this->info('Welcome to the Organogram Package Installation!');
-<<<<<<< HEAD
-        
-        // Ask if user wants frontend
-=======
-
->>>>>>> d0767ef1debaab9034c94637e64674374542e4fb
         $includeFrontend = $this->choice(
             'What would you like to install?',
             ['API only', 'API + Frontend'],
             0
         );
-<<<<<<< HEAD
-        
-=======
-
->>>>>>> d0767ef1debaab9034c94637e64674374542e4fb
         // Run migrations
         $this->call('migrate');
         
@@ -53,20 +38,6 @@ class InstallOrganogram extends Command
         $this->call('db:seed', [
             '--class' => 'Sanwarul\Organogram\Database\Seeders\DatabaseSeeder'
         ]);
-<<<<<<< HEAD
-        
-        if ($includeFrontend === 'API + Frontend') {
-            $this->installFrontend();
-        }
-        
-        $this->info('Installation completed successfully!');
-    }
-    
-    protected function installFrontend()
-    {
-        $this->info('Setting up frontend...');
-        
-=======
 
         $this->publishModels();
 
@@ -81,26 +52,15 @@ class InstallOrganogram extends Command
     {
         $this->info('Setting up frontend...');
 
->>>>>>> d0767ef1debaab9034c94637e64674374542e4fb
         // Create frontend directory if it doesn't exist
         $frontendPath = base_path('organogram-frontend');
         if (!File::exists($frontendPath)) {
             File::makeDirectory($frontendPath);
         }
-<<<<<<< HEAD
-        
-        // Copy frontend files from package to project
-        File::copyDirectory(__DIR__.'/../../frontend', $frontendPath);
-        
-        // Update .env file with API URL
-        $this->updateEnvFile();
-        
-=======
 
         // Copy frontend files from package to project
         File::copyDirectory(__DIR__ . '/../../frontend', $frontendPath);
 
->>>>>>> d0767ef1debaab9034c94637e64674374542e4fb
         $this->info('Frontend installed successfully!');
         $this->info('To run the frontend:');
         $this->info('1. cd organogram-frontend');
@@ -108,65 +68,6 @@ class InstallOrganogram extends Command
         $this->info('3. create a local.env file with your environment variables');
         $this->info('4. npm run dev');
     }
-<<<<<<< HEAD
-    
-    protected function updateEnvFile()
-    {
-        $envPath = base_path('.env');
-        $envContent = File::get($envPath);
-        $envContent = str_replace('APP_URL=http://localhost', 'APP_URL=http://localhost/organogram-frontend', $envContent);
-        File::put($envPath, $envContent);
-    }
-}
-=======
-
-    // protected function checkMigrations()
-    // {
-    //     $this->info('Checking for existing migrations...');
-    //     $migrationsPath = database_path('migrations');
-
-    //     if (File::exists($migrationsPath)) {
-    //         $this->info('Migrations already exist. Skipping migration step.');
-    //     } else {
-    //         $this->info('No migrations found. Proceeding with migration.');
-    //         $this->call('migrate');
-    //     }
-    // }
-
-    // protected function checkSeeders()
-    // {
-    //     $this->info('Checking for existing seeders...');
-    //     $seedersPath = database_path('seeders');
-
-    //     if (File::exists($seedersPath)) {
-    //         $this->info('Seeders already exist. Skipping seeder step.');
-    //     } else {
-    //         $this->info('No seeders found. Proceeding with seeding.');
-    //         $this->call('db:seed', [
-    //             '--class' => 'Sanwarul\Organogram\Database\Seeders\DatabaseSeeder'
-    //         ]);
-    //     }
-    // }
-
-    // protected function publishMigrations()
-    // {
-    //     $this->info('Publishing migrations...');
-    //     $this->call('vendor:publish', [
-    //         '--provider' => 'Sanwarul\Organogram\Providers\OrganogramServiceProvider',
-    //         '--tag' => 'organogram-migrations'
-    //     ]);
-    //     $this->info('Migrations published successfully!');
-    // }
-
-    // protected function publishSeeders()
-    // {
-    //     $this->info('Publishing seeders...');
-    //     $this->call('vendor:publish', [
-    //         '--provider' => 'Sanwarul\Organogram\Providers\OrganogramServiceProvider',
-    //         '--tag' => 'organogram-seeders'
-    //     ]);
-    //     $this->info('Seeders published successfully!');
-    // }
 
     protected function publishModels()
     {
@@ -214,4 +115,3 @@ class InstallOrganogram extends Command
         }
     }
 }
->>>>>>> d0767ef1debaab9034c94637e64674374542e4fb
